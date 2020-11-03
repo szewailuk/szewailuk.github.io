a8e3980b368bd14d9678e1f0f96804250680132d
JackyLuk <jkluk@sfu.ca>
2020-11-03 12:59:57 -0800

    Testing Progresive Load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,3 @@
-// $(document).ready(function() {
-//     $('.slider').each(function(index) {
-//         $(this).append('<div class="slideroverlay"></div>')
-//     });
-
-
-
-
-//     $(".btn-simple").each(function(index) {
-
-//         $(this).addClass('off');
-
-//         $(this).find('.clickarea').on("mousedown", function() {
-//             $(this).parent().toggleClass('pressed');
-//         });
-//         $(this).find('.clickarea').on("mouseup", function() {
-//             $(this).parent().toggleClass('pressed');
-//         });
-
-
-
-
-
-//     });
-
-//     $('.slideshow').cycle({
-//         fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-//     });
-
-
-// });
-
 jQuery(function($) {
     if($('#mainpageslider').length) {
         
@@ -42,12 +10,6 @@ jQuery(function($) {
     }
 
     var $hamburger = $(".hamburger");
-      // $hamburger.on("click", function(e) {
-      //   if (!$("#navbarToggleExternalContent").hasClass('show')) {
-      //       $hamburger.toggleClass("is-active");
-      //   }
-      //   // Do something else, like open/close menu
-      // });
 
     $('#navbarToggleExternalContent').on('shown.bs.collapse', function() {
     }).on('show.bs.collapse', function() {
@@ -62,46 +24,9 @@ jQuery(function($) {
         }
     });
 
-
-    // $.fatNav();
-
-    // $( ".navbar-toggler" ).on('click', function() {
-    //     var testHeight = $(".header").height();
-    //     $( ".cycle-slideshow" ).css("top",testHeight);
-    // });
-
-    // $( ".navbar-toggler" ).on('click', function() {
-    //     if ($("#navbarToggleExternalContent").hasClass('show')) {
-    //         var testHeight = $(".header").height();
-    //         $( ".cycle-slideshow" ).css("top",testHeight);
-    //     }
-    // });
-
-    // $('#navbarToggleExternalContent').on('shown.bs.collapse', function() {
-    //     var testHeight = $(".header").height();
-    //     $( ".cycle-slideshow" ).css("top",testHeight);
-    // }).on('show.bs.collapse', function() {
-    //     console.log("show");
-    // });
-
-    // $(".variable").slick({
-    //     lazyLoad: 'ondemand',
-    //     dots: false,
-    //     infinite: false,
-    //     variableWidth: true,
-    //     adaptiveHeight: true,
-    //     focusOnSelect: true,
-    //     responsive: [
-    //             {
-    //                 breakpoint: 500,
-    //                 settings: "unslick"
-    //             }
-    //         ]
-    // });
-
     function createSlick(){
         $('.variable').not('.slick-initialized').slick({
-            lazyLoad: 'ondemand',
+            lazyLoad: 'progressive',
             dots: false,
             // infinite: false,
             variableWidth: true,
@@ -118,33 +43,6 @@ jQuery(function($) {
     }
     createSlick();
     $(window).on( 'resize', createSlick );
-
-
-
-
-    // if($('.slider-iv').length) {
-        
-    //     $('.slider-iv').each(function() {
-    //         var $this = $(this);
-    //         $this.cycle({
-    //             fx:     'fade', 
-    //             speed:  500, 
-    //             timeout:  0,
-                
-    //             // use slideshow as the transition trigger
-    //             next:     $this,
-                
-    //             // update caption div after transition
-    //             after:    function(curr,next,opts) {
-    //                 var s = 'Image ' + (opts.currSlide + 1) + ' / ' + opts.slideCount;
-    //                 $(opts.caption).html(s);
-    //             },
-                
-    //             // capture the caption div in the options object
-    //             caption:  $this.closest('.item-image-set').find('p.cycle-caption')
-    //         });
-    //     });
-    // }   
 
 });
 
